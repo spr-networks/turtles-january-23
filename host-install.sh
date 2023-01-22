ef0c212104548126421960d62e32577527c51399
Alex Rad <37549748+lts-rad@users.noreply.github.com>
2023-01-22 07:12:02 -0800

    Add wireless-regdb dependency on host

--- host-install.sh
+++ host-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get install -y docker.io docker-compose linux-modules-extra-`uname -r` iw
+apt-get install -y docker.io docker-compose linux-modules-extra-`uname -r` wireless-regdb iw
